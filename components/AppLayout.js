2bb6445465a21403d2e014ba002592d9cc1ed8b3
junlyang-home <eomlyang@gmail.com>
2019-12-06 15:44:23 +0900

    first commit

---
+++ components/AppLayout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Menu} from "antd";
+
+const AppLayout = () => {
+    return (
+        <div>
+            <Menu>
+                <Menu.Item>메뉴1</Menu.Item>
+                <Menu.Item>메뉴2</Menu.Item>
+                <Menu.Item>메뉴3</Menu.Item>
+            </Menu>
+        </div>
+    )
+}
+
+export default AppLayout
